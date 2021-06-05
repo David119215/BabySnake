f3fdf76cdebc9190618f1b047c7d40c59fbd4468
David Atkinson <david.atkinson317@gmail.com>
2021-06-05 13:15:36 -0400

    add a function

--- babysnake.py
+++ babysnake.py
@@ -18,27 +18,9 @@ https://en.wikipedia.org/wiki/Shebang_(Unix)
 
 import os  # This line lets us use the "print" function provided to us by Python
 
-# This is a function.  A block of code that can be REUSED
-def new_function():
-    print("  Running new_function()")
-    print("  Doing lots of stuff....")
-    print("  \"Returning\" from new_function()")
-    print("\\")
-    print("  BABYSNAKE!!!!!")
-
 
 # main() is a special function that is always run first
 def main():
-    print(" Running main")
-    print(" About to \"call\" new_function()")
-    new_function()
-    print(" Finished running new_function()")
-
-    print(" Let's \"call\" new_function() again!")
-    new_function()
-    print(" Finished running new_function()")
-    print(" \"Returning\" from main()")
-
     x = 2
     y = f(x)
     print(" f(x) where x == 2 returned ", y)
@@ -48,6 +30,5 @@ def f(var):
 
 # This is code that will "call" main()
 if __name__ == "__main__":
-    print("About to \"call\" main()")  # Notice the slash before the quotation mark.  WHY??????
     main()
-    print("Finished running main()")
+
